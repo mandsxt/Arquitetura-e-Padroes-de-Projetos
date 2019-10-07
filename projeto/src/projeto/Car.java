d4aa660d8a84e7ba7218e753402c9aa262bcbc31
mandsxt <36248355+mandsxt@users.noreply.github.com>
2019-10-07 14:58:37 -0300

    projeto

---
+++ projeto/src/projeto/Car.java
@@ -0,0 +1,14 @@
+package projeto;
+
+class Car {
+    int wheel;
+    String color;
+    public Car(int wheel, String color) {
+        this.wheel=wheel;
+        this.color=color;
+    }
+    @Override
+    public String toString() {
+        return "Car [wheels = " + wheel+ ", color = " + color + "]";
+    }
+}
